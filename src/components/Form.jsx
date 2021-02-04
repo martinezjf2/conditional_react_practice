3f8eb619a5b55c7214899cc3e8aa18e488c9ece8
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-04 14:42:42 -0500

    show login if isRegistered is false

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Input from "./Input";
 
-function Form() {
+function Form(props) {
   return (
     <form className="form">
       <Input type="text" placeholder="Username" />
       <Input type="password" placeholder="Password" />
       <Input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      <button type="submit">{props.isRegistered ? "Register" : "Login"}</button>
     </form>
   );
 }
